da5ee0e6a933221b4ae97a6a65b9f916e2fa30fe
jinook929 <jinook929@gmail.com>
2021-05-03 23:20:44 -0400

    refactor getNotices method

--- _flatiron_project4_frontend/notice.js
+++ _flatiron_project4_frontend/notice.js
@@ -10,97 +10,53 @@ class Notice {
     this.user_id = notice.user_id
   }
 
-  // static getNotice(notice, i) {
-  //   // set icon by category
-  //   let icon
-  //   if (notice.category === "Today's Works") {
-  //     icon = `<i class="fas fa-calendar-day category-icon"></i>`
-  //   } else if (notice.category === "Meetings") {
-  //     icon = `<i class="fas fa-users category-icon"></i>`
-  //   } else if (notice.category === "Tips") {
-  //     icon = `<i class="far fa-lightbulb category-icon"></i>`
-  //   } else {
-  //     icon = `<i class="fab fa-rocketchat category-icon"></i>`
-  //   }
-  //   // create card html
-  //   const card = `
-  //   <div id="n_${notice.id}" class="accordion-item">
-  //   <h2 class="accordion-header" id="notice_${notice.id}">
-  //     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_${notice.id}" aria-expanded="false" aria-controls="collapse_${notice.id}">
-  //       ${icon} <span class="notice-title">${notice.title}</span>
-  //     </button>
-  //   </h2>
-  //   <div id="collapse_${notice.id}" class="accordion-collapse collapse" aria-labelledby="heading_${notice.id}" data-bs-parent="#board">
-  //     <div class="accordion-body">
-  //       <div>${notice.description}<div>
-  //       <div id="edit-form-${notice.id}" class="off">
-  //       <div class="comments"></div>
-  //     </div>
-  //   </div>
-  //   `
-  //   let token = currentUser()
-  //   if (!!token) {
-  //     axios.post(`${url}/sessions/token`, { token: token })
-  //       .then(res => {
-  //         if (notice.user_id === res.data.id) {
-  //           const descriptionBody = document.querySelector(`#collapse_${notice.id}`)
-  //           const flexDiv = document.createElement("div")
-  //           const editBtn = document.createElement("button")
-  //           const deleteBtn = document.createElement("button")
-  //           const editForm = document.querySelector(`#edit-form-${notice.id}`)
-  //           flexDiv.className = "d-flex gap-1 mt-3 me-3"
-  //           editBtn.innerHTML = "Edit"
-  //           editBtn.classList.add("btn", "btn-warning", "btn-sm", "ms-auto")
-  //           deleteBtn.innerHTML = "Delete"
-  //           deleteBtn.classList.add("btn", "btn-danger", "btn-sm")
-  //           flexDiv.append(editBtn)
-  //           flexDiv.append(deleteBtn)
-  //           descriptionBody.prepend(flexDiv)
-  //           editForm.innerHTML = `
-  //         <form id="edit-post-form-${notice.id}" class="edit-post-form">
-  //           <label class="new-form-label" for="title">TITLE</label><br>
-  //           <input class="edit-form-input" type="text" name="title" id="title" required value="${notice.title}">
-  //           <br>
-  //           <label class="new-form-label" for="content">DESCRIPTION</label><br>
-  //           <textarea class="edit-form-input" type="text" name="content" id="content" rows="5" required>${notice.description}</textarea>
-  //           <br>
-  //           <label class="new-form-label" for="edit-category">CATEGORY</label>
-  //           <select name="edit-category" id="edit-category">
-  //             <option value="Today's Works" ${(notice.category === "Today's Works") ? "selected" : ""}>Today's Works</option>
-  //             <option value="Meetings" ${(notice.category === "Meetings") ? "selected" : ""}>Meetings</option>
-  //             <option value="Tips" ${(notice.category === "Tips") ? "selected" : ""}>Tips</option>
-  //             <option value="etc" ${(notice.category === "etc") ? "selected" : ""}>etc</option>
-  //           </select>
-  //           <br>
-  //           <input class="edit-form-btn" type="submit" value="SUBMIT">
-  //         </form>
-  //         `
-  //           editBtn.addEventListener("click", e => {
-  //             editForm.classList.toggle("off")
-  //           })
-  //           editForm.addEventListener("submit", e => {
-  //             e.preventDefault()
-  //             console.log("submit!!!")
-  //             Notice.editNotice(notice)
-  //           })
-  //           deleteBtn.addEventListener("click", e => {
-  //             console.log("Delete???")
-  //             console.log(notice)
-  //             Notice.deleteNotice(notice)
-  //           })
-  //         }
-  //       })
-  //   }
+  static getNotice(notice, i) {
+    // set icon by category
+    let icon
+    if (notice.category === "Today's Works") {
+      icon = `<i class="fas fa-calendar-day category-icon"></i>`
+    } else if (notice.category === "Meetings") {
+      icon = `<i class="fas fa-users category-icon"></i>`
+    } else if (notice.category === "Tips") {
+      icon = `<i class="far fa-lightbulb category-icon"></i>`
+    } else {
+      icon = `<i class="fab fa-rocketchat category-icon"></i>`
+    }
+    // create card html
+    const card = `
+    <div id="n_${notice.id}" class="accordion-item">
+    <h2 class="accordion-header" id="notice_${notice.id}">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_${notice.id}" aria-expanded="false" aria-controls="collapse_${notice.id}">
+        ${icon} <span class="notice-title">${notice.title}</span>
+      </button>
+    </h2>
+    <div id="collapse_${notice.id}" class="accordion-collapse collapse" aria-labelledby="heading_${notice.id}" data-bs-parent="#board">
+      <div class="accordion-body">
+        <div>${notice.description}<div>
+        <div id="edit-form-${notice.id}" class="off">
+        <div class="comments"></div>
+      </div>
+    </div>
+    `
+    let token = currentUser()
+    if (!!token) {
+      axios.post(`${url}/sessions/token`, { token: token })
+        .then(res => {
+          if (notice.user_id === res.data.id) {
+            Notice.addEditDeleteBtn(notice)
+          }
+        })
+    }
 
-  //   // append card to display
-  //   board.innerHTML += card
+    // append card to display
+    board.innerHTML += card
 
-  //   // set the first card to open
-  //   if (i === 0) {
-  //     document.querySelector("h2 button").classList.remove("collapsed")
-  //     document.querySelector(`#collapse_${notice.id}`).classList.add("show")
-  //   }
-  // }
+    // set the first card to open
+    if (i === 0) {
+      document.querySelector("h2 button").classList.remove("collapsed")
+      document.querySelector(`#collapse_${notice.id}`).classList.add("show")
+    }
+  }
 
   // get all notices from backend and display them
   static getNotices() {
@@ -116,95 +72,6 @@ class Notice {
         // make them into cards html 
         notices.forEach((notice, i) => {
           Notice.getNotice(notice, i)
-          // set icon by category
-          let icon
-          if (notice.category === "Today's Works") {
-            icon = `<i class="fas fa-calendar-day category-icon"></i>`
-          } else if (notice.category === "Meetings") {
-            icon = `<i class="fas fa-users category-icon"></i>`
-          } else if (notice.category === "Tips") {
-            icon = `<i class="far fa-lightbulb category-icon"></i>`
-          } else {
-            icon = `<i class="fab fa-rocketchat category-icon"></i>`
-          }
-          // create card html
-          const card = `
-            <div id="n_${notice.id}" class="accordion-item">
-            <h2 class="accordion-header" id="notice_${notice.id}">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_${notice.id}" aria-expanded="false" aria-controls="collapse_${notice.id}">
-                ${icon} <span class="notice-title">${notice.title}</span>
-              </button>
-            </h2>
-            <div id="collapse_${notice.id}" class="accordion-collapse collapse" aria-labelledby="heading_${notice.id}" data-bs-parent="#board">
-              <div class="accordion-body">
-                <div>${notice.description}<div>
-                <div id="edit-form-${notice.id}" class="off">
-                <div class="comments"></div>
-              </div>
-            </div>
-            `
-          let token = currentUser()
-          if (!!token) {
-            axios.post(`${url}/sessions/token`, { token: token })
-              .then(res => {
-                if (notice.user_id === res.data.id) {
-                  addEditDeleteBtn(notice)
-                  // const descriptionBody = document.querySelector(`#collapse_${notice.id}`)
-                  // const flexDiv = document.createElement("div")
-                  // const editBtn = document.createElement("button")
-                  // const deleteBtn = document.createElement("button")
-                  // const editForm = document.querySelector(`#edit-form-${notice.id}`)
-                  // flexDiv.className = "d-flex gap-1 mt-3 me-3"
-                  // editBtn.innerHTML = "Edit"
-                  // editBtn.classList.add("btn", "btn-warning", "btn-sm", "ms-auto")
-                  // deleteBtn.innerHTML = "Delete"
-                  // deleteBtn.classList.add("btn", "btn-danger", "btn-sm")
-                  // flexDiv.append(editBtn)
-                  // flexDiv.append(deleteBtn)
-                  // descriptionBody.prepend(flexDiv)
-                  // editForm.innerHTML = `
-                  // <form id="edit-post-form-${notice.id}" class="edit-post-form">
-                  //   <label class="new-form-label" for="title">TITLE</label><br>
-                  //   <input class="edit-form-input" type="text" name="title" id="title" required value="${notice.title}">
-                  //   <br>
-                  //   <label class="new-form-label" for="content">DESCRIPTION</label><br>
-                  //   <textarea class="edit-form-input" type="text" name="content" id="content" rows="5" required>${notice.description}</textarea>
-                  //   <br>
-                  //   <label class="new-form-label" for="edit-category">CATEGORY</label>
-                  //   <select name="edit-category" id="edit-category">
-                  //     <option value="Today's Works" ${(notice.category === "Today's Works") ? "selected" : ""}>Today's Works</option>
-                  //     <option value="Meetings" ${(notice.category === "Meetings") ? "selected" : ""}>Meetings</option>
-                  //     <option value="Tips" ${(notice.category === "Tips") ? "selected" : ""}>Tips</option>
-                  //     <option value="etc" ${(notice.category === "etc") ? "selected" : ""}>etc</option>
-                  //   </select>
-                  //   <br>
-                  //   <input class="edit-form-btn" type="submit" value="SUBMIT">
-                  // </form>
-                  // `
-                  // editBtn.addEventListener("click", e => {
-                  //   editForm.classList.toggle("off")
-                  // })
-                  // editForm.addEventListener("submit", e => {
-                  //   e.preventDefault()
-                  //   console.log("submit!!!")
-                  //   Notice.editNotice(notice)
-                  // })
-                  // deleteBtn.addEventListener("click", e => {
-                  //   console.log("Delete???")
-                  //   Notice.deleteNotice(notice)
-                  // })
-                }
-              })
-          }
-
-          // append card to display
-          board.innerHTML += card
-
-          // set the first card to open
-          if (i === 0) {
-            document.querySelector("h2 button").classList.remove("collapsed")
-            document.querySelector(`#collapse_${notice.id}`).classList.add("show")
-          }
         })
       })
   }
@@ -270,7 +137,7 @@ class Notice {
 
   static editNotice(notice) {
     let editPostForm = document.querySelector(`#edit-post-form-${notice.id}`)
-    fetch(`${url} / notices / ${notice.id}`, {
+    fetch(`${url}/notices/${notice.id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -295,23 +162,23 @@ class Notice {
   // display new notice form
   static displayNewForm() {
     newNotice.innerHTML = `
-      < h3 id = "new-post" class= "lead" > Post New Notice < br > (Click Here)</h3 >
-        <form id="new-post-form" class="off">
-          <label class="new-form-label" for="title">TITLE</label><br>
-            <input class="new-form-input" type="text" name="title" id="title" required>
-              <br>
-                <label class="new-form-label" for="content">DESCRIPTION</label><br>
-                  <textarea class="new-form-input" type="text" name="content" id="content" rows="5" required></textarea>
-                  <br>
-                    <label class="new-form-label" for="new-category">CATEGORY</label>
-                    <select name="new-category" id="new-category">
-                      <option value="Today's Works">Today's Works</option>
-                      <option value="Meetings">Meetings</option>
-                      <option value="Tips">Tips</option>
-                      <option value="etc" selected>etc</option>
-                    </select>
-                    <br>
-                      <input class="new-form-btn" type="submit" value="SUBMIT">
+    <h3 id = "new-post" class= "lead" > Post New Notice <br> (Click Here)</h3>
+    <form id="new-post-form" class="off">
+      <label class="new-form-label" for="title">TITLE</label><br>
+      <input class="new-form-input" type="text" name="title" id="title" required>
+      <br>
+      <label class="new-form-label" for="content">DESCRIPTION</label><br>
+      <textarea class="new-form-input" type="text" name="content" id="content" rows="5" required></textarea>
+      <br>
+      <label class="new-form-label" for="new-category">CATEGORY</label>
+      <select name="new-category" id="new-category">
+        <option value="Today's Works">Today's Works</option>
+        <option value="Meetings">Meetings</option>
+        <option value="Tips">Tips</option>
+        <option value="etc" selected>etc</option>
+      </select>
+      <br>
+      <input class="new-form-btn" type="submit" value="SUBMIT">
     </form>
     `
     const newPost = document.querySelector("#new-post")
